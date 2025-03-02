3c3d5e724076d07bdad42c30f0e3515c819ef0b7
ChristiannYa <christian.ylopez02@gmail.com>
2025-03-02 17:17:39 -0600

    setup

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+import './styles/Logo.scss';
+import Logo from './components/Logo';
+
+const App = () => {
+  return (
+    <div className="bg-[#1c1c1c] wh-screen p-4">
+      <div className="wh-full relative">
+        <div className="absolute-middle-left flexcol-center gap-y-2">
+          <Logo />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Logo.jsx
@@ -0,0 +1,20 @@
+const Logo = () => {
+  return (
+    <div className="text-white font-antonDisplay border">
+      <div className="flexrow-center">
+        <div className="text-2xl">
+          <p>The Learn</p>
+          <p>Learning</p>
+          <p>Academy</p>
+        </div>
+        <div className="text-lg text-start">
+          <p>Unlock</p>
+          <p>Your</p>
+          <p>Potential</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Logo;
